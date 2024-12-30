f627faf889ed2922f329642be07004f77903d44f
cdaringe <cdaringe@gmail.com>
2024-12-29 20:48:16 -0800

    wee

---
+++ .github/isobot.ts
@@ -0,0 +1,17 @@
+type Pipeline = any;
+
+export const pipeline: Pipeline = (
+  stream,
+  {
+    rxjs: {
+      operators: { filter, mergeMap },
+    },
+  }
+) =>
+  stream.pipe(
+    filter((evt) => evt.name === "issue_comment"),
+    mergeMap((evt) => evt.tk.gh.withPR(evt, evt.payload.issue.id)),
+    mergeMap((evt) => evt.tk.gh.withPRComments(evt, evt.ctx.pr.id)),
+    mergeMap((evt) => evt.tk.gh.approvePR(evt, evt.ctx.pr.id)),
+    mergeMap((evt) => evt.tk.gh.mergePR(evt, evt.ctx.pr.id))
+  );
